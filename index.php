3487f6efc2ab3d98902d6a5881d680a564909a9e
afperea <afperea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-08-21 16:47:07 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-copyrightpro/tags/1.3@147856 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ index.php
@@ -0,0 +1,36 @@
+<?php
+/*
+      Plugin Name: WP-CopyRightPro
+      Plugin URI: http://puydi.net/blog/wp-copyrightpro-plug-in-para-wordpress/
+      Description: WP-CopyRightPro is a plug-in that prevents the copying of texts and images from your blog.
+      Version: 1.3
+      Author: Andres Felipe Perea V
+      Author URI: http://puydi.net/
+*/
+
+/*
+Copyright 2009  PUYDI, IN  (http://www.puydi.net/)
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details: http://www.gnu.org/licenses/gpl.txt
+*/
+   
+function copyrighthead(){
+include ('script.htm');
+}
+function copyrightpuydi(){
+echo '<small>This site is protected by <a href="http://puydi.net/blog/wp-copyrightpro-plug-in-para-wordpress/">WP-CopyRightPro</a></small>';
+}
+
+/* Añadir comando wordpress */
+add_action('wp_head','copyrighthead');
+add_action('wp_footer','copyrightpuydi');
+
+?>
