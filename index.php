95f948cd8adb08a9654cd461a97d2a8eb987f90a
afperea <afperea@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2009-12-16 18:39:23 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-copyrightpro/trunk@183526 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- index.php
+++ index.php
@@ -2,8 +2,8 @@
 /*
       Plugin Name: WP-CopyRightPro
       Plugin URI: http://puydi.net/blog/wp-copyrightpro-plug-in-para-wordpress/
-      Description: WP-CopyRightPro is a plug-in that prevents the copying of texts and images from your blog.
-      Version: 1.5
+      Description: WP-CopyRightPro is a plug-in that prevents the copying of texts and images from your blog, if you install this plug-in, your content of wordpress will be protected.
+      Version: 2.0
       Author: Andres Felipe Perea V.
       Author URI: http://puydi.net/
 */
@@ -21,15 +21,76 @@ but WITHOUT ANY WARRANTY; without even the implied warranty of
 MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details: http://www.gnu.org/licenses/gpl.txt
 */
-   
+
+/* INSTALL AND UNISTALL PLUG-IN */
+function copyproinstall() {
+	global $wpdb;
+ 
+	$sql = 'CREATE TABLE `wp_copyrightpro` (
+			`copy_click` VARCHAR( 1 ) NOT NULL ,
+			`copy_selection` VARCHAR( 1 ) NOT NULL ,
+			`copy_iframe` VARCHAR( 1 ) NOT NULL ,
+			`copy_drop` VARCHAR( 1 ) NOT NULL
+			) ENGINE = MYISAM DEFAULT CHARSET=utf8';
+ 
+	$wpdb->query($sql);
+	$wpdb->query('INSERT INTO `'.wp_copyrightpro.'` SET copy_click = "y", copy_selection = "y", copy_iframe = "n", copy_drop = "y"');
+}
+
+function copyprouninstall() {
+	global $wpdb;
+ 
+	$sql = "DROP TABLE `wp_copyrightpro`";
+	$wpdb->query($sql);
+}
+
+
+/* FUNCIONES DE PROTECCION */ 
 function copyrighthead(){
 include ('script.htm');
 }
+
 function copyrightpuydi(){
 echo '<small>This site is protected by <a href="http://puydi.net/blog/wp-copyrightpro-plug-in-para-wordpress/">WP-CopyRightPro</a></small>';
 }
 
+/* PANEL DE CONTROL */
+
+function update_copypro($clickpro, $selecpro, $iframepro, $droppro){
+global $wpdb;
+	if($clickpro==""){
+	$clickpro="n";
+	}
+	if($selecpro==""){
+	$selecpro="n";
+	}
+	if($iframepro==""){
+	$iframepro="n";
+	}
+	if($droppro==""){
+	$droppro="n";
+	}
+$wpdb->query("UPDATE wp_copyrightpro SET copy_click = '$clickpro', copy_selection = '$selecpro', copy_iframe = '$iframepro', copy_drop = '$droppro'");
+}
+
+function panel_copypro() {
+	include ('panel.php');
+}
+
+function about_copypro() {
+	include ('about.html');
+}
+
+function config_copypro() {
+	add_menu_page('CopyRightPro Panel', 'CopyRightPro', 'administrator', 'wp-copyrightpro/panel.php', 'panel_copypro', plugins_url('wp-copyrightpro/images/Computer.gif'));
+	add_submenu_page( 'wp-copyrightpro/panel.php', 'About CopyRighPro', 'About', 'administrator', 'about-copyrightpro', 'about_copypro');
+}
+
 /* Añadir comando wordpress */
+register_activation_hook(__FILE__,'copyproinstall'); //gancho para instalar
+register_deactivation_hook(__FILE__,'copyprouninstall'); //gancho para desinstalar
+
+add_action('admin_menu','config_copypro');
 add_action('wp_head','copyrighthead');
 add_action('wp_footer','copyrightpuydi');
 
--- readme.txt
+++ readme.txt
@@ -4,7 +4,7 @@ Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_i
 Tags: copyright, images, gallery, prevent copy, text, disable right click, prevent select text, wordpress, content, protect
 Requires at least: 2.7
 Tested up to: 2.8.6
-Stable tag: 1.5
+Stable tag: 2.0
 
 If you install WP-CopyRightPro, your content of wordpress will be protected.
 
@@ -13,14 +13,19 @@ If you install WP-CopyRightPro, your content of wordpress will be protected.
 **WP-CopyRightPro** is a plug-in that prevents the copying of texts and images from your blog,
 if you install this plug-in, your content of wordpress will be protected.
 
+Wp-CopyrightPro is a plug-in developed by **[Andres Perea V](http://batallamillonaria.com/)**. in order to minimize the copying of your website content. This is not a complete solution, but it will avoid 90% of attempts to copy its contents.
+
 = This plug-in will =
 
 * **Disable selection of text**
 * **Disable right click on your Wordpress**
 * **Protects from iframes**
+* **Protects from drag and drop images**
 * **WP-CopyRightPro doesn't have problems with search engines**
 
 
+**[Demo](http://quehayparahacer.com/)**
+**[Support](http://puydi.net/blog/wp-copyrightpro-plug-in-para-wordpress/)**
 **[Download now](http://downloads.wordpress.org/plugin/wp-copyrightpro.zip)**
 
 
@@ -52,7 +57,22 @@ This section describes how to install the plugin and get it working.
 1. Copyright Plug-in **WP-CopyRightPro**
 
 
+== FAQS ==
+
+= Wp-CopyRightPro is bad for the SEO? =
+This plug-in is developed in PHP and javascript, for this reason the plug-in does not affect search engines, it only affects the user's browser that tries to copy your content.
+
+= Wp-CopyrightPro detects the hotlink? =
+When activating 100% of the protections, in less than a week, Wp-CopyRightPro can reveal sites that are using your images, just by logging into Google.com images section type this (site:yoursite.com) and google will show the sites that are using your images.
+
+**To see the plug-in in action you can enter [here](http://quehayparahacer.com/).**
+**For questions, suggestions, please enter our Official Site [here](http://puydi.net/blog/wp-copyrightpro-plug-in-para-wordpress/).**
+
 == Changelog ==
+
+= 2.0 =
+* new control panel, drag and drop protection
+  Tested up to: 2.8.6
 
 = 1.5 =
 * Now, charged fast and Tested up to: 2.8.6
